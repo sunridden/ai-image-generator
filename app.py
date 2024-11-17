b8e68d58ba34b37e592731c7714fc279254222e8
kevin <kevinszeto9449@gmail.com>
2024-11-17 15:01:11 -0800

    Read user input

--- app.py
+++ app.py
@@ -1,4 +1,27 @@
 def startProgram():
     programRunning = True
 
+    while(programRunning == True):
+        print("Paste your script below and press 'Enter', or press '1' or 'stop' to exit the program")
+        userInput = input()
+
+        #Exit the program
+        if (userInput.lower() == 'stop' or userInput == '1'):
+            programRunning = False
+            break
+
+        print('\nYour input is:\n\n' + userInput)
+        print('\nDo you want to proceed with the following script? Press "0" or "y" to proceed or "1" to exit the program')
+        nextUserInput = input()
+
+        #Exit the program
+        if (nextUserInput.lower() == 'stop' or nextUserInput == '1'):
+            programRunning = False
+            break
+        if (nextUserInput.lower() == 'y' or nextUserInput.lower() == 'yes' or nextUserInput == 1):
+            #Proceed to enter parameters
+            programRunning = False
+        
+    
+startProgram()
     
